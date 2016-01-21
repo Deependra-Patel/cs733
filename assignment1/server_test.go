acd57eb9daf04b98f42b583daa8d0b0269e01778
deependra <pateldeependra06@gmail.com>
2016-01-21 23:44:50 +0530

    Adding test case with special chars in content

--- assignment1/server_test.go
+++ assignment1/server_test.go
@@ -10,14 +10,13 @@ import (
 	"time"
 )
 
-
-// Simple serial check of getting and setting
-func TestRead(t *testing.T) {
+func TestServer(t *testing.T) {
 	go serverMain()
 	time.Sleep(1 * time.Second) // one second is enough time for the server to start
 	name := "hi.txt"
 	contents := "bye"
 	contents2 := "second"
+	contents3 := "xxcasabc write a 3\\adf cas delete hi.txt"
 	exptime := 300000
 	smallExptime := 1
 	conn, err := net.Dial("tcp", "localhost:8080")
@@ -73,12 +72,31 @@ func TestRead(t *testing.T) {
 	if err != nil{
 		t.Error("Non-numeric version found")
 	}
-	fmt.Fprintf(conn, "delete %v\r\n", name) // try a read now
+	fmt.Fprintf(conn, "delete %v\r\n", name)
 	scanner.Scan()
 	expect(t, scanner.Text(), "OK")
-	fmt.Fprintf(conn, "read %v\r\n", name) // try a read now
+	fmt.Fprintf(conn, "read %v\r\n", name)
 	scanner.Scan()
 	expect(t, scanner.Text(), "ERR_FILE_NOT_FOUND")
+
+	//Testing with gibberish values in file content
+	fmt.Fprintf(conn, "write %v %v\r\n%v\r\n", name, len(contents3), contents3)
+	scanner.Scan() // read first line
+	resp = scanner.Text() // extract the text from the buffer
+	arr = strings.Split(resp, " ") // split into OK and <version>
+	expect(t, arr[0], "OK")
+	version, err = strconv.ParseInt(arr[1], 10, 64) // parse version as number
+	if err != nil {
+		t.Error("Non-numeric version found")
+	}
+	fmt.Fprintf(conn, "read %v\r\n", name) // try a read now
+	scanner.Scan()
+	arr = strings.Split(scanner.Text(), " ")
+	expect(t, arr[0], "CONTENTS")
+	expect(t, arr[1], fmt.Sprintf("%v", version)) // expect only accepts strings, convert int version to string
+	expect(t, arr[2], fmt.Sprintf("%v", len(contents3)))
+	scanner.Scan()
+	expect(t, scanner.Text(), contents3)
 }
 
 // Useful testing function
