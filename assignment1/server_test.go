f205cde01d470795bd21ff8419acbab98f29f630
Deependra-Patel <pateldeependra06@gmail.com>
2016-01-25 16:53:36 +0530

    Organised test cases to different functions

--- assignment1/server_test.go
+++ assignment1/server_test.go
@@ -10,20 +10,18 @@ import (
 	"time"
 )
 
-func TestServer(t *testing.T) {
-	go serverMain()
-	time.Sleep(1 * time.Second) // one second is enough time for the server to start
+func TestMain(m *testing.T) {
+	go serverMain()   // launch the server as a goroutine.
+	time.Sleep(1 * time.Second)
+}
+func TestReadWrite(t *testing.T) {
 	name := "hi.txt"
 	contents := "bye"
-	contents2 := "second"
-	contents3 := "xxcasabc write a 3\\adf cas delete hi.txt"
 	exptime := 300000
-	smallExptime := 1
 	conn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
 		t.Error(err.Error()) // report error through testing framework
 	}
-
 	scanner := bufio.NewScanner(conn)
 
 	// Test write+read
@@ -45,31 +43,61 @@ func TestServer(t *testing.T) {
 	expect(t, arr[2], fmt.Sprintf("%v", len(contents)))
 	scanner.Scan()
 	expect(t, scanner.Text(), contents)
+}
 
-	//Testing CAS with expiry
-	fmt.Fprintf(conn, "cas %v %v %v %v\r\n%v\r\n", name, version, len(contents2), smallExptime, contents2)
-	scanner.Scan()
-	resp = scanner.Text()
-	arr = strings.Split(resp, " ")
+func TestCAS(t *testing.T) {
+	name := "hi2.txt"
+	contents := "first"
+	contents2 := "second"
+	exptime := 1000
+	conn, err := net.Dial("tcp", "localhost:8080")
+	scanner := bufio.NewScanner(conn)
+
+	if err != nil {
+		t.Error(err.Error()) // report error through testing framework
+	}
+	fmt.Fprintf(conn, "write %v %v %v\r\n%v\r\n", name, len(contents), exptime, contents)
+	scanner.Scan() // read first line
+	resp := scanner.Text() // extract the text from the buffer
+	arr := strings.Split(resp, " ") // split into OK and <version>
 	expect(t, arr[0], "OK")
-	version, err = strconv.ParseInt(arr[1], 10, 64)
-	if err != nil{
+	version, err := strconv.ParseInt(arr[1], 10, 64) // parse version as number
+	if err != nil {
 		t.Error("Non-numeric version found")
 	}
-	time.Sleep(time.Second*3)
-	fmt.Fprintf(conn, "read %v\r\n", name) // try a read now
-	scanner.Scan()
-	arr = strings.Split(scanner.Text(), " ")
-	expect(t, arr[0], "ERR_FILE_NOT_FOUND")
-
-	//Testing delete
-	fmt.Fprintf(conn, "write %v %v\r\n%v\r\n", name, len(contents2), contents2)
+	fmt.Fprintf(conn, "cas %v %v %v %v\r\n%v\r\n", name, version, len(contents2), exptime, contents2)
 	scanner.Scan()
 	resp = scanner.Text()
 	arr = strings.Split(resp, " ")
 	expect(t, arr[0], "OK")
 	version, err = strconv.ParseInt(arr[1], 10, 64)
-	if err != nil{
+	if err != nil {
+		t.Error("Non-numeric version found")
+	}
+	fmt.Fprintf(conn, "read %v\r\n", name)
+	scanner.Scan()
+	arr = strings.Split(scanner.Text(), " ")
+	expect(t, arr[0], "CONTENTS")
+	expect(t, arr[1], fmt.Sprintf("%v", version))
+	expect(t, arr[2], fmt.Sprintf("%v", len(contents2)))
+	scanner.Scan()
+	expect(t, scanner.Text(), contents2)
+}
+func TestDeleteAndExpiry(t *testing.T) {
+	name := "hi.txt"
+	contents := "something"
+	exptime := 1
+	conn, err := net.Dial("tcp", "localhost:8080")
+	scanner := bufio.NewScanner(conn)
+
+	//Testing delete
+	fmt.Fprintf(conn, "write %v %v\r\n%v\r\n", name, len(contents), contents)
+	scanner.Scan()
+	resp := scanner.Text()
+	arr := strings.Split(resp, " ")
+	expect(t, arr[0], "OK")
+	_, err = strconv.ParseInt(arr[1], 10, 64)
+	if err != nil {
 		t.Error("Non-numeric version found")
 	}
 	fmt.Fprintf(conn, "delete %v\r\n", name)
@@ -79,32 +107,13 @@ func TestServer(t *testing.T) {
 	scanner.Scan()
 	expect(t, scanner.Text(), "ERR_FILE_NOT_FOUND")
 
-	//Testing with gibberish values in file content
-	fmt.Fprintf(conn, "write %v %v\r\n%v\r\n", name, len(contents3), contents3)
-	scanner.Scan() // read first line
-	resp = scanner.Text() // extract the text from the buffer
-	arr = strings.Split(resp, " ") // split into OK and <version>
-	expect(t, arr[0], "OK")
-	version, err = strconv.ParseInt(arr[1], 10, 64) // parse version as number
-	if err != nil {
-		t.Error("Non-numeric version found")
-	}
-	fmt.Fprintf(conn, "read %v\r\n", name) // try a read now
-	scanner.Scan()
-	arr = strings.Split(scanner.Text(), " ")
-	expect(t, arr[0], "CONTENTS")
-	expect(t, arr[1], fmt.Sprintf("%v", version)) // expect only accepts strings, convert int version to string
-	expect(t, arr[2], fmt.Sprintf("%v", len(contents3)))
-	scanner.Scan()
-	expect(t, scanner.Text(), contents3)
-
 	//Delete when file is expired should give ERR_FILE_NOT_FOUND
-	fmt.Fprintf(conn, "write %v %v %v\r\n%v\r\n", name, len(contents2), smallExptime, contents2)
+	fmt.Fprintf(conn, "write %v %v %v\r\n%v\r\n", name, len(contents), exptime, contents)
 	scanner.Scan()
 	resp = scanner.Text()
 	arr = strings.Split(resp, " ")
 	expect(t, arr[0], "OK")
-	version, err = strconv.ParseInt(arr[1], 10, 64)
+	_, err = strconv.ParseInt(arr[1], 10, 64)
 	if err != nil{
 		t.Error("Non-numeric version found")
 	}
@@ -112,6 +121,34 @@ func TestServer(t *testing.T) {
 	fmt.Fprintf(conn, "delete %v\r\n", name)
 	scanner.Scan()
 	expect(t, scanner.Text(), "ERR_FILE_NOT_FOUND")
+}
+func TestFileContent(t *testing.T) {
+	name := "hi.txt"
+	contents3 := "xxcasabc write a 3\\adf cas delete hi.txt"
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		t.Error(err.Error()) // report error through testing framework
+	}
+
+	scanner := bufio.NewScanner(conn)
+	//Testing with gibberish values in file content
+	fmt.Fprintf(conn, "write %v %v\r\n%v\r\n", name, len(contents3), contents3)
+	scanner.Scan() // read first line
+	resp := scanner.Text() // extract the text from the buffer
+	arr := strings.Split(resp, " ") // split into OK and <version>
+	expect(t, arr[0], "OK")
+	version, err := strconv.ParseInt(arr[1], 10, 64) // parse version as number
+	if err != nil {
+		t.Error("Non-numeric version found")
+	}
+	fmt.Fprintf(conn, "read %v\r\n", name) // try a read now
+	scanner.Scan()
+	arr = strings.Split(scanner.Text(), " ")
+	expect(t, arr[0], "CONTENTS")
+	expect(t, arr[1], fmt.Sprintf("%v", version))
+	expect(t, arr[2], fmt.Sprintf("%v", len(contents3)))
+	scanner.Scan()
+	expect(t, scanner.Text(), contents3)
 }
 
 // Useful testing function
